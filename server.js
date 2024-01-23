df8e8969867523c545458f90c29fb815679784b6
Mezekr W <mezi.edu@tuta.io>
2024-01-23 10:25:46 +0100

    add routing via url and fs node models

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((req, res) => {
-	res.writeHead('200', { 'Content-type': 'text/html' });
-	res.write('Hello Movie app!');
-	res.end();
+	let q = url.parse(req.url, true);
+	let filePath = '';
+
+	if (q.pathname.includes('documentation')) {
+		filePath = __dirname + '/documentation.html';
+	} else {
+		filePath = 'index.html';
+	}
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			res.writeHead(404, { 'Content-type': 'text/html' });
+			res.end('404 not found');
+		}
+		res.writeHead('200', { 'Content-type': 'text/html' });
+		res.write(data);
+		res.end();
+	});
 }).listen(8080);
 
 console.log('Movie app with Node test server is running on Port 8080.');
