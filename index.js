90bdc8536315613e05d46ca2ede0df7556b215aa
Mezekr W <mezi.edu@tuta.io>
2024-01-27 08:21:26 +0100

    add movies endpoint

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ const app = express();
 
 console.log('this is test');
 
-app.get('/', (req, res) => res.send('This is express test.'));
+const topMovies = [
+	{
+		id: 1,
+		title: 'Avatar',
+		year: 2009,
+		director: 'James Cameron',
+		writer: 'James Cameron',
+	},
+	{
+		id: 2,
+		title: 'The Avengers',
+		year: 2012,
+		director: 'Joss Whedon',
+		writer: 'Joss Whedon',
+	},
+	{
+		id: 3,
+		title: 'The Wolf of Wall Street',
+		year: 2013,
+		director: 'Martin Scorsese',
+		writer: 'Terence Winter (screenplay)',
+	},
+	{
+		id: 4,
+		title: 'I Am Legend',
+		year: 2007,
+		director: 'Francis Lawrence',
+		writer: 'Mark Protosevich (screenplay), Akiva Goldsman (screenplay)',
+	},
+	{
+		id: 5,
+		title: 'The Irishman',
+		year: 2019,
+		director: 'Martin Scorsese',
+		writer: 'Steven Zaillian (screenplay), Charles Brandt (book)',
+	},
+];
+
+app.get('/', (req, res) => res.send('Welcome to MeziFlix movies app'));
+
+app.get('/movies', (req, res) => res.json(topMovies));
 
 app.listen(8080, () => console.log('Your app is listening on port 8080.'));
