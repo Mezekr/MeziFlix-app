6be6ac8ca6a8a4a7bacc7f4f6255af8dd7e58a8e
Mezekr W <mezi.edu@tuta.io>
2024-01-27 07:40:00 +0100

    create express object

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+
+console.log('this is test');
+
+app.get('/', (req, res) => res.send('This is express test.'));
+
+app.listen(8080, () => console.log('Your app is listening on port 8080.'));
