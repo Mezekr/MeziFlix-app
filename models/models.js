fbdbc2edacfa02989b71085b389bde8f7748be54
Mezekr W <mezi.edu@tuta.io>
2024-02-10 20:23:31 +0100

    add timestamps to movie and user schemas

--- models/models.js
+++ models/models.js
@@ -1,42 +1,57 @@
 const mongoose = require('mongoose');
 
-const movieSchema = mongoose.Schema({
-	Title: {
-		type: String,
-		required: [true, 'Please include the Title of the movie'],
+const movieSchema = mongoose.Schema(
+	{
+		Title: {
+			type: String,
+			required: [true, 'Please include the Title of the movie'],
+		},
+		Description: {
+			type: String,
+			required: [true, 'Please include the Description of the movie'],
+		},
+		Genre: {
+			Name: String,
+			Description: String,
+		},
+		Director: {
+			Name: String,
+			Bio: String,
+		},
+		Actors: [String],
+		ImagePath: String,
+		Featured: Boolean,
 	},
-	Description: {
-		type: String,
-		required: [true, 'Please include the Description of the movie'],
-	},
-	Genre: {
-		Name: String,
-		Description: String,
-	},
-	Director: {
-		Name: String,
-		Bio: String,
-	},
-	Actors: [String],
-	ImagePath: String,
-	Featured: Boolean,
-});
+	{
+		timestamps: true,
+	}
+);
 
 // Users Mongoose Models Schema
 
-const userSchema = mongoose.Schema({
-	Username: { type: String, required: [true, 'Please enter a username.'] },
-	Password: {
-		type: String,
-		required: [
-			true,
-			'The password must not be empty. Please enter a password.',
+const userSchema = mongoose.Schema(
+	{
+		Username: {
+			type: String,
+			required: [true, 'Please enter a username.'],
+		},
+		Password: {
+			type: String,
+			required: [
+				true,
+				'The password must not be empty. Please enter a password.',
+			],
+		},
+		Email: {
+			type: String,
+			required: [true, 'Please fill in an email address.'],
+		},
+		Birthday: Date,
+		FavouriteMovies: [
+			{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
 		],
 	},
-	Email: {
-		type: String,
-		required: [true, 'Please fill in an email address.'],
-	},
-	Birthday: Date,
-	FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-});
+	{
+		timestamps: true,
+	}
+);
